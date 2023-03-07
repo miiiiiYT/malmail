32b37a3a5aa3c76338d4697ccaa206739f09424e
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2023-03-07 17:05:06 +0100

    Added a way to save variables and load them dynamically

---
+++ src/vars/basic.py
@@ -0,0 +1,2 @@
+from src import *
+about="MalMail 0.0.1\n\nAuthor: miiiiiYT\n\nProject Page at https://github.com/miiiiiYT/malmail\n\n(c) 2023 Finn Petersen\nLicensed under the keepRight License v1.1."
