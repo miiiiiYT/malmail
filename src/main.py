b9cc3d2b3496ae5dc715e54a0c1d606543b41691
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2023-03-08 23:11:59 +0100

    you can now add accounts via a popup, although nothing gets checked yet

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from src import *
 from src.gui.gui import Gui
 from src.vars.basic import about
+from src.functions.accounts import Account
 
 
 while True:
@@ -30,7 +31,8 @@ while True:
                 [sg.Text('Password: '),sg.Input(key='-ADD-PASSWORD-', password_char='\u2022')],
                 [sg.Push(),sg.OK(),sg.Cancel()]
             ]
-            sg.Window('Create Account - MalMail',layout,modal=True).read(close=True)
+            acc_creator_event, acc_creator_values = sg.Window('Create Account - MalMail',layout,modal=True).read(close=True)#type: ignore
+            Account(email=acc_creator_values['-ADD-EMAIL-'],imapaddr=acc_creator_values['-ADD-IMAP-'],smtpaddr=acc_creator_values['-ADD-SMTP-'],password=acc_creator_values['-ADD-PASSWORD-'],imapport=acc_creator_values['-ADD-IMAP-PORT-'],smtpport=acc_creator_values['-ADD-SMTP-PORT-']).create()
 
     if event == 'Add-Acc':
         print(event)
