1013d4e1c7ec1bb57461bb3848f54aec5d3ca9b2
miiiiiYT <41822967+miiiiiYT@users.noreply.github.com>
2023-03-10 14:41:54 +0100

    Added a first run sequence

--- run.py
+++ run.py
@@ -1,1 +1,16 @@
+from json import load,dump
+
+with open('data/config.json') as f:
+	config = load(f)
+	f.close()
+if not config['first_run']:
+		# execute everything that needs to be done for first launch
+		with open('data/accounts.json','x') as f:
+			f.write('{}')
+			f.close()
+		config['first_run'] = True
+		with open('data/config.json','w') as f:
+			dump(config,f)
+			f.close()
+
 import src.main
